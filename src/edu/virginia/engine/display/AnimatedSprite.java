1d74f48ae0c7732ca5abbe61e4b87dfafa5bad03
Gene Rhee <gkr6sy@virginia.edu>
2016-03-20 12:50:17 -0400

    changes animatedSprites

--- src/edu/virginia/engine/display/AnimatedSprite.java
+++ src/edu/virginia/engine/display/AnimatedSprite.java
@@ -1,108 +1,97 @@
 package edu.virginia.engine.display;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.*;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashMap;
 
 public class AnimatedSprite extends Sprite {
-	private ArrayList<BufferedImage> frames = new ArrayList<BufferedImage>();
-	private int startIndex = 0;
-	private int endIndex = 0;
-	private int currentFrame = 0;
-	private int defaultIndex;
-	private int frameCount = 0;
-	private int w0 = -1;
-	private int w1 = 0;
-	private int j0 = -1;
-	private int j1 = 0;
-	public boolean jumping = false;
+	private ArrayList<BufferedImage> animations;
+	private HashMap<String, ArrayList<Integer>> animStats;
+	private ArrayList<Double> animationSpeeds;
+	private int currentFrame;
+	private int startIndex;
+	private int endIndex;
+	private int animationCounter;
+	private boolean halted;
 
-	public AnimatedSprite(String id, String imageFileName) {
-		super(id, imageFileName);
-		imageFileName = imageFileName.substring(0, imageFileName.length() - 4);
-		Path dir = Paths.get("./resources");
-		ArrayList<String> files = new ArrayList<String>();
-		try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
-			for (Path file : stream) {
-				String fileName = file.toString().replace("."+File.separator+"resources"+File.separator, "");
-				files.add(fileName);
-			}
-		} catch (IOException | DirectoryIteratorException x) {
-			// IOException can never be thrown by the iteration.
-			// In this snippet, it can only be thrown by newDirectoryStream.
-			System.err.println(x);
-		}
-		Collections.sort(files);
-		for (String fileName : files) {
-//			System.out.println(fileName);
-			if (fileName.contains(imageFileName)) {
-				if (fileName.equals(imageFileName+".png")) {
-					frames.add(readImage(fileName));
-//					System.out.println("default"+fileName);
-					defaultIndex = frames.size() - 1;
-					currentFrame = defaultIndex;
-					// This is our default image
-				}
-				if (fileName.contains("walk")) {
-					frames.add(readImage(fileName));
-					if (w0 == -1)
-						w0 = frames.size() - 1;
-					w1 = frames.size() - 1;
-				}
-				if (fileName.contains("jump")) {
-					frames.add(readImage(fileName));
-					if (j0 == -1)
-						j0 = frames.size() - 1;
-					j1 = frames.size() - 1;
-				}
-			}
-		}
-//		System.out.println(w0+" "+w1);
+	private long startTime;
+	private long allotedTime;
+
+
+	public AnimatedSprite(String id, String defaultImg) {
+		super(id, defaultImg);
+		animations = new ArrayList<BufferedImage>();
+		animStats = new HashMap<>();
+		this.animationSpeeds = new ArrayList<>();
+		currentFrame = -1;
+		animationCounter = 0;
+		animations.add(super.readImage(defaultImg));
 	}
 
-	public void animate(String animation) {
-		switch (animation) {
-		case "jump":
-			startIndex = j0;
-			endIndex = j1+1;
-			currentFrame = startIndex;
-			break;
-		case "walkL":
-			this.setScaleX(-1*Math.abs(this.getScaleX()));
-			startIndex = w0;
-			endIndex = w1+1;
-			currentFrame = startIndex;
-			break;	
-		case "walkR":
-			this.setScaleX(Math.abs(this.getScaleX()));
-			startIndex = w0;
-			endIndex = w1+1;
-			currentFrame = startIndex;
-			break;
-		}
+
+	public void haltAnim(boolean bool) {
+		this.halted = bool;
 	}
 
-	@Override
-	public void update(ArrayList<String> pressedKeys) {
-		super.update(pressedKeys);
-			if(frames.size()>0){
-				this.setImage(frames.get(currentFrame));
-				if (currentFrame < endIndex && frameCount == 10) {
-					currentFrame++;
-					frameCount = 0;
-				}
-				if (currentFrame < endIndex && frameCount < 10) {
-					frameCount++;
-				}
-				if (currentFrame == endIndex) {
-					frameCount = 0;
-					startIndex = defaultIndex;
-					endIndex = defaultIndex;
-					currentFrame = defaultIndex;
-				}
+
+	/*
+        returns true when done
+     */
+	public boolean animate(String anim) {
+		boolean ret = false;
+		if (this.halted == false) {
+			if (startTime == 0) {
+				startIndex = animStats.get(anim).get(0);
+				endIndex = animStats.get(anim).get(1);
+				currentFrame = startIndex;
+				startTime = System.nanoTime();
+				allotedTime = (long)(animationSpeeds.get(animStats.get(anim).get(2)) *
+						1000000000 / ((endIndex - startIndex)));
 			}
+			// return the animation based on the elapsed time
+			if (System.nanoTime() - startTime < allotedTime) {
+
+			}
+			else if (currentFrame == endIndex) {
+				currentFrame = 0;
+				startTime = 0;
+				ret = true;
+			}
+			else {
+				startTime = System.nanoTime();
+				currentFrame += 1;
+			}
+
 		}
+		super.setImage(animations.get(currentFrame));
+		return ret;
+	}
+
+
+	/*
+        Length is given in seconds
+     */
+	public void readAnimation(String anim, ArrayList<String> animation, double length) {
+		int startFrame = this.animations.size();
+		for (int i = 0; i < animation.size(); i++) {
+			// add each image into the animation
+			this.animations.add(super.readImage(animation.get(i)));
+		}
+		ArrayList<Integer> stats = new ArrayList<Integer>();
+		stats.add(startFrame);
+		stats.add(this.animations.size() - 1); // end frame
+
+		animationSpeeds.add(length);
+		stats.add(animationCounter);
+		this.animStats.put(anim, stats);
+	}
+
+	public void setAnimSpeed(String anim, double spd) {
+		animationSpeeds.set(animStats.get(anim).get(2), spd);
+	}
+
+
+	public boolean isHalted() {
+		return halted;
+	}
 }
