14c0576928d91c1eedc0ca5b2cd45f35731d080c
viggykuppu <viggykuppu@gmail.com>
2016-03-25 12:40:33 -0400

    XML for first level, begin working on parser

---
+++ src/edu/virginia/engine/util/LevelGenerator.java
@@ -0,0 +1,14 @@
+package edu.virginia.engine.util;
+
+import com.sun.org.apache.xerces.internal.parsers.XMLParser;
+
+/**
+ * Created by vignesh on 3/25/16.
+ */
+public class LevelGenerator {
+    public LevelGenerator(){
+
+    }
+
+
+}
