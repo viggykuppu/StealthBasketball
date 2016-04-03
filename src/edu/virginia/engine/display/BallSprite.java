c9089e119777be8a1ad1fecf5a7ef37fda71dda7
Gene Rhee <gkr6sy@virginia.edu>
2016-04-03 15:09:15 -0400

    Insert getCollisionNormal for BallSprite
    getCollisionNormal takes the offending GridSprite
    and gets its bounds and compares them to the ball center.
    It returns an Enum with the direction the ball should go.

--- src/edu/virginia/engine/display/BallSprite.java
+++ src/edu/virginia/engine/display/BallSprite.java
@@ -4,6 +4,7 @@ import edu.virginia.engine.tween.Tween;
 import edu.virginia.engine.tween.TweenJuggler;
 import edu.virginia.engine.tween.TweenTransitions;
 import edu.virginia.engine.tween.TweenableParams;
+import edu.virginia.engine.util.Direction;
 import javafx.scene.shape.Ellipse;
 
 import java.awt.*;
@@ -35,10 +36,11 @@ public class BallSprite extends Sprite {
         for (DisplayObject obj : spriteList) {
             GridSprite s = (GridSprite) obj;
             if (this.collidesWith(s) && s.getId() != "Player") {
-                rebound();
+                this.getCollisionNormal(s);
             }
         }
     }
+
 
     public void pathToGridPoint(Point gridDest, long timems) {
         Point endPosition = new Point(gridDest);
@@ -53,10 +55,49 @@ public class BallSprite extends Sprite {
     }
 
     /*
-        Either stops the ball or reverses vector
+        Calculates which rectangle side the ball is closest to
+        Returns: Direction Enum
      */
-    public void rebound() {
+    public Direction getCollisionNormal(GridSprite s) {
+        Rectangle area = s.getHitbox().getBounds();
 
+        // the sides of the rectangle
+        double left = area.getX();
+        double right = left + area.getWidth();
+        double up = area.getY();
+        double down = up + area.getHeight();
+
+        // get the closest side of the rectangle
+        Rectangle ball = this.getHitCircle().getBounds();
+
+        // get the center of ball
+        double centerX = ball.getX() + (ball.getWidth() / 2);
+        double centerY = ball.getY() + (ball.getHeight() / 2);
+
+        // do the math
+        double distLeft = Math.abs(left - centerX);
+        double distRight = Math.abs(right - centerX);
+        double distUp = Math.abs(up - centerY);
+        double distDown = Math.abs(down - centerY);
+
+        Direction ret = Direction.LEFT;
+        double smallest = distLeft;
+
+        if (distRight < smallest) {
+            ret = Direction.RIGHT;
+            smallest = distRight;
+        }
+
+        if (distUp < smallest) {
+            ret = Direction.UP;
+            smallest = distUp;
+        }
+
+        if (distDown < smallest) {
+            ret = Direction.DOWN;
+        }
+
+        return ret;
     }
 
     public Point getPlayerOffset() {
